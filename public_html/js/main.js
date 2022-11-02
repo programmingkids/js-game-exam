6fca3ea425072c2749c7390102b8bde88f1ff09d
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-11-02 05:28:45 +0000

    fix for main

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -17,16 +17,16 @@ mainScene.create = function () {
     this.createUI();
     
     // プレイヤーの攻撃グループ作成
-    this.createShootGroup();
+    
     
     // 敵グループの作成
-    this.createEnemyGroup();
+    
     
     // アイテムグループの作成
-    this.createItemGroup();
+    
     
     // プレイヤー用のパーティクル作成
-    this.createParticle();
+    
 };
 
 mainScene.update = function() {
@@ -140,41 +140,25 @@ mainScene.gameOver = function() {
 
 mainScene.createShootGroup = function() {
     // プレイヤーの攻撃グループ
-    this.shootGroup = this.physics.add.group();
+    
+    
+    
 };
 
 mainScene.createShoot = function() {
     // プレイヤーの攻撃処理作成
     // this.bulletsが0になると発射できない
     // プレイヤーの位置から発射
-    if(this.bullets <= 0) {
-        return;
-    }
-    this.bullets--;
-    this.bulletsText.setText("Bullets:" + this.bullets);
     
-    var x = this.player.x;
-    var y = this.player.y;
     
-    var shot = this.shootGroup.create(x, y, 'shot');
-    shot.setDisplaySize(30, 70);
-    shot.setVelocityY(-500);
+    
 };
 
 mainScene.createEnemyGroup = function() {
     // 敵のグループ作成
-    this.enemyGroup = this.physics.add.group();
     
-    // 敵の衝突処理設定
-    this.physics.add.overlap(this.player, this.enemyGroup, this.hitEnemy, null, this);
-    this.physics.add.overlap(this.shootGroup, this.enemyGroup, this.hitShoot, null, this);
     
-    this.enemyTimer = this.time.addEvent({
-        delay: 1000,
-        callback: this.createEnemy,
-        loop: true,
-        callbackScope: this,
-    });
+    
 };
 
 mainScene.createEnemy = function() {
@@ -182,30 +166,16 @@ mainScene.createEnemy = function() {
     // X座標は100から700のランダムな座標
     // Y座標は0で固定位置
     // 下方向のみにに加速
-    var x = Phaser.Math.RND.between(100, 700);
-    var y = 0;
     
-    var enemyImage = Phaser.Math.RND.pick(this.enemyType);
-    var enemy = this.enemyGroup.create(x, y, enemyImage);
     
-    enemy.setDisplaySize(60, 60);
-    enemy.setVelocityY(150);
     
-    this.createEnemyParticle(enemy);
 };
 
 mainScene.createItemGroup = function() {
     // アイテムグループ作成
-    this.itemGroup = this.physics.add.group();
     
-    this.physics.add.overlap(this.player, this.itemGroup, this.hitItem, null, this);
     
-    this.itemTimer = this.time.addEvent({
-        delay: 4000,
-        callback: this.createItem,
-        loop: true,
-        callbackScope : this,
-    });
+    
 };
 
 mainScene.createItem = function() {
@@ -214,73 +184,45 @@ mainScene.createItem = function() {
     // Y座標は0で固定位置
     // 下方向のみにに加速
     
-    var x = Phaser.Math.RND.between(100, 700);
-    var y = 0;
     
-    var item = this.itemGroup.create(x, y, 'item');
-    item.setDisplaySize(50, 50);
-    item.setVelocityY(100);
+    
 };
 
 mainScene.hitEnemy = function(player, enemy) {
     // プレイヤーと敵が衝突したときの処理
     // this.player.lifeが減少する
     // this.player.lifeが0になるとゲームオーバー
-    if( this.isGameOver ) {
-        return;
-    }
-    enemy.destroy();
-    this.player.life--;
-    this.lifeText.setText('Life:' + this.player.life);
     
-    if(this.player.life <= 0) {
-        this.isGameOver = true;
-        this.emitter.start();
-        this.player.setVisible(false);
-        this.gameOver();
-    }
+    
+    
 };
 
 mainScene.hitShoot = function(shot, enemy) {
     // プレイヤーの攻撃と敵が衝突したときの処理
     // スコアに5加算
-    shot.destroy();
-    enemy.emitter.start();
-    enemy.destroy();
-    this.score += 5;
-    this.scoreText.setText('Score:' + this.score);
+    
+    
+    
 };
 
 mainScene.hitItem = function(player, item) {
     // プレイヤーとアイテムが衝突したときの処理
     // this.bulletsに5加算
-    item.destroy();
-    this.bullets += 5;
-    this.bulletsText.setText("Bullets:" + this.bullets);
+    
+    
+    
 };
 
 mainScene.createParticle = function() {
     // プレイヤーの爆発パーティクル作成
-    var particles = this.add.particles('explosion1');
-    this.emitter = particles.createEmitter({
-        speed: 200,
-        maxParticles: 20,
-        blendMode: 'ADD',
-        follow: this.player,
-    });
-    // 最初はパーティクルは停止
-    this.emitter.stop();
+    
+    
+    
 };
 
 mainScene.createEnemyParticle = function(enemy) {
     // 敵の爆発パーティクル作成
-    var particles = this.add.particles('explosion2');
-    enemy.emitter = particles.createEmitter({
-        speed: 200,
-        maxParticles: 5,
-        blendMode: 'ADD',
-        follow: enemy,
-    });
-    // 最初はパーティクルは停止
-    enemy.emitter.stop();
+    
+    
+    
 };
